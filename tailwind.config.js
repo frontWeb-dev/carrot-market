6bb2e35cb1872193b3da182e089975478a3f50e3
frontweb-dev <frontend_web@kakao.com>
2023-03-21 16:13:36 +0900

    feat: 초기 셋팅

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // 어디서 사용할 것인지 알려주기
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+};
