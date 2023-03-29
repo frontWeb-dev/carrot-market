4c01a1a500395623059672bba4866ece7614129f
frontweb-dev <frontend_web@kakao.com>
2023-03-29 11:14:39 +0900

    feat: twilio

--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from 'twilio';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { withHandler, ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(request: NextApiRequest, response: NextApiResponse<ResponseType>) {
   const { phone, email } = request.body;
@@ -8,6 +11,7 @@ async function handler(request: NextApiRequest, response: NextApiResponse<Respon
 
   if (!user) return response.status(400).json({ ok: false });
 
+  const payload = String(Math.random()).substring(2, 8);
   const token = await client.token.create({
     data: {
       payload: String(Math.random()).substring(2, 8),
@@ -25,6 +29,14 @@ async function handler(request: NextApiRequest, response: NextApiResponse<Respon
     },
   });
 
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.PHONE_NUMBER!,
+      body: `Your login token is ${payload}.`,
+    });
+    console.log(message);
+  }
   return response.json({
     ok: true,
   });
