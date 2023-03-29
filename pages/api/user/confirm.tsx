ce2464c3b2974472d621031c17f2eb662dbd065d
frontweb-dev <frontend_web@kakao.com>
2023-03-29 13:36:02 +0900

    feat: Serverless Session
    iron-session

--- pages/api/user/confirm.tsx
+++ pages/api/user/confirm.tsx
@@ -1,11 +1,26 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { withHandler, ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
 
 async function handler(request: NextApiRequest, response: NextApiResponse<ResponseType>) {
   const { token } = request.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
 
+  if (!exists) response.status(404).end();
+  request.session.user = {
+    id: exists?.userId,
+  };
+
+  await request.session.save();
   response.status(200).end();
 }
-export default withHandler('POST', handler);
+
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrot-session',
+  password: process.env.IRON_PW!,
+});
